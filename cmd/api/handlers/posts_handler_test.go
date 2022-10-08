78a67d2da413f3844153196982f9f9d134d9cd17
Abiodun Azeez <iamhabbeboy@gmail.com>
2022-10-09 00:23:57 +0100

    add e2e test

---
+++ cmd/api/handlers/posts_handler_test.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type EndToEndSuite struct {
+	suite.Suite
+}
+
+func TestEndToEndSuuite(t *testing.T) {
+	suite.Run(t, new(EndToEndSuite))
+}
+
+func (s *EndToEndSuite) TestPostHandler() {
+	c := http.Client{}
+	r, _ := c.Get("http://localhost:1323/posts")
+	s.Equal(http.StatusOK, r.StatusCode)
+}
